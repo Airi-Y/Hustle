03fe27eb20ac9c055de194cb74130f5f478a70c3
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2022-12-17 14:08:58 +0800

    Complete CodeChumCircle with Exception Handling

--- CodechumCircle/src/CircleGUI.java
+++ CodechumCircle/src/CircleGUI.java
@@ -1,4 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class NegativeRadiusException extends Exception {
+    @Override
+    public String getMessage() {
+        return "The radius should not be a negative number. Try again!";
+    }
+}
 
 public class CircleGUI extends JFrame{
     private JPanel pnlMain;
@@ -8,11 +17,56 @@ public class CircleGUI extends JFrame{
     private JButton btnClear;
     private JButton btnCompute;
 
-    public static void main(String[] args) {
-        CircleGUI app = new CircleGUI();
-        app.setContentPane(app.pnlMain);
-        app.setSize(700, 700);
-        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        app.setVisible(true);
+    public CircleGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double radius = Double.parseDouble(tfRadius.getText());
+                    if (radius < 0)
+                        throw new NegativeRadiusException();
+
+
+                    double area = circleArea(radius);
+                    double circ = circleCircumference(radius);
+
+                    tfArea.setText(String.format("%,.2f", area));
+                    tfCircumference.setText(String.format("%,.2f", circ));
+                }
+                catch (NumberFormatException err) {
+                    JOptionPane.showMessageDialog(pnlMain, "Invalid input. Please enter a valid number.");
+                }
+                catch (NegativeRadiusException err) {
+                    JOptionPane.showMessageDialog(pnlMain, err.getMessage());
+                }
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearInputs();
+            }
+        });
+    }
+
+    public JPanel getPnlMain() {
+        return pnlMain;
+    }
+
+    public double circleArea(double r) {
+        // pi * r^2
+        return (Math.PI * (Math.pow(r, 2)));
+    }
+
+    public double circleCircumference(double r) {
+        // 2 pi r
+        return 2 * (Math.PI * r);
+    }
+
+    public void clearInputs() {
+        tfArea.setText("");
+        tfRadius.setText("");
+        tfCircumference.setText("");
     }
 }
--- CodechumCircle/src/Main.java
+++ CodechumCircle/src/Main.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            CircleGUI app = new CircleGUI();
+            app.setContentPane(app.getPnlMain());
+            app.setSize(700, 700);
+            app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            app.setVisible(true);
     }
 }
